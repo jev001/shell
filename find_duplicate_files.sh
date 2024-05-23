89c1aa8b993847b550e1c9f0305f3fb5aecb8d51
jonah <jonah@x411>
2024-05-23 21:38:05 +0800

    dd

--- find_duplicate_files.sh
+++ find_duplicate_files.sh
@@ -12,14 +12,14 @@ while getopts ":i:o:rp:" opt; do
     p) find_path="$OPTARG";;
     d) depth="$OPTARG";;
     o) output_file="$OPTARG";;
-    \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+    \?) echo "Usage: $0 [-p input_path] [-o output_file] [-d depth]" >&2 exit 1;;
   esac
 done
 
-if [ -z "$output_file" ] ; then
-  echo "Usage: $0 [-p input_path] [-o output_file] [-d depth]" >&2
-  exit 1
-fi
+# if [ -z "$output_file" ] ; then
+#   echo "Usage: $0 [-p input_path] [-o output_file] [-d depth]" >&2
+#   exit 1
+# fi
 
 allfiles=""
 # 检查必需参数是否已设置
@@ -37,6 +37,9 @@ echo md5_all="\`comm  <(echo \$allfiles) <(echo \$uniqfiles) -2 -3 | cut -c -32\
 md5_all=`comm  <(echo $allfiles) <(echo $uniqfiles) -2 -3 | cut -c -32|tr '\n' '\0'`
 
 #echo "uniq_all_files=\`echo \$md5_all |xargs -I {}  grep {} <(echo \$allfiles) |tr '\n' '\0' \`"
-
-echo $md5_all |xargs -I {}  grep {} <(echo $allfiles) >$output_file
-echo "结果已保存到 $output_file 文件。"
+if [ -z "$depth" ] ; then
+  echo $md5_all |xargs -I {}  grep {} <(echo $allfiles)
+else
+  echo $md5_all |xargs -I {}  grep {} <(echo $allfiles) >$output_file
+  echo "结果已保存到 $output_file 文件。"
+fi

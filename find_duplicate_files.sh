089c43f3a29248cc4d00a4e07c3cc65b2529f478
jonah <jonah@x411>
2024-05-23 21:36:22 +0800

    ddd

--- find_duplicate_files.sh
+++ find_duplicate_files.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 #Program
 
-#!/bin/bash
-
 # 定义默认值
 find_path="."
 depth=""
+output_file=""
 
 # 使用 getopts 处理参数
 while getopts ":i:o:rp:" opt; do
   case $opt in
     p) find_path="$OPTARG";;
     d) depth="$OPTARG";;
+    o) output_file="$OPTARG";;
     \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
   esac
 done
 
+if [ -z "$output_file" ] ; then
+  echo "Usage: $0 [-p input_path] [-o output_file] [-d depth]" >&2
+  exit 1
+fi
+
 allfiles=""
 # 检查必需参数是否已设置
 if [ -z "$depth" ] ; then
-  echo "allfiles=\`find .  -type f -print0 | xargs -0 md5sum | sort\`"
-  allfiles=`find .  -type f -print0 | xargs -0 md5sum | sort`
+  echo "allfiles=\`find .  -type f -print0 | xargs -0 md5sum | sort |tr '\n' '\0' \`"
+  allfiles=`find .  -type f -print0 | xargs -0 md5sum | sort | tr '\n' '\0'`
 else
-  echo "allfiles=\`find . maxdepth \$depth -type f -print0 | xargs -0 md5sum | sort\`"
-  allfiles=`find . maxdepth $depth -type f -print0 | xargs -0 md5sum | sort`
+  echo "allfiles=\`find . maxdepth \$depth -type f -print0 | xargs -0 md5sum | sort | tr '\n' '\0' \`"
+  allfiles=`find . maxdepth $depth -type f -print0 | xargs -0 md5sum | sort |tr '\n' '\0' `
 fi
-echo "uniqfiles=\`echo \$allfiles | uniq -w 32\`"
-uniqfiles=`echo $allfiles | uniq -w 32`
-echo md5_all="\`comm  <(echo \$allfiles) <(echo \$uniqfiles) -2 -3 | cut -c -32\`"
-md5_all=`comm  <(echo $allfiles) <(echo $uniqfiles) -2 -3 | cut -c -32`
-echo "uniq_all_files=\`echo \$md5_all |xargs -I {}  grep {} <(echo \$allfiles) \`"
-uniq_all_files=`echo $md5_all |xargs -I {}  grep {} <(echo $allfiles) `
-echo "\`echo \$uniq_all_files\`"
-echo $uniq_all_files
+echo "uniqfiles=\`echo \$allfiles | uniq -w 32 |tr '\n' '\0' \`"
+uniqfiles=`echo $allfiles | uniq -w 32|tr '\n' '\0'`
+
+echo md5_all="\`comm  <(echo \$allfiles) <(echo \$uniqfiles) -2 -3 | cut -c -32\ | tr '\n' '\0' \`"
+md5_all=`comm  <(echo $allfiles) <(echo $uniqfiles) -2 -3 | cut -c -32|tr '\n' '\0'`
+
+#echo "uniq_all_files=\`echo \$md5_all |xargs -I {}  grep {} <(echo \$allfiles) |tr '\n' '\0' \`"
+
+echo $md5_all |xargs -I {}  grep {} <(echo $allfiles) >$output_file
+echo "结果已保存到 $output_file 文件。"

8fbe7c429484c3bc985995ec6e7fafdea2634b57
jonah <jonah@x411>
2024-05-27 20:25:19 +0800

    20240527

--- find_duplicate_files.sh
+++ find_duplicate_files.sh
@@ -26,16 +26,26 @@ allfiles=""
 if [ -z "$depth" ] ; then
   echo "allfiles=\`find .  -type f -print0 | xargs -0 md5sum | sort |tr '\n' '\0' \`"
   allfiles=`find . -type f -print0 | xargs -0 md5sum | sort | tr '\n' '\0'`
+  find . -type f -print0 | xargs -0 md5sum | sort > 1.txt
 else
   echo "allfiles=\`find . maxdepth \$depth -type f -print0 | xargs -0 md5sum | sort | tr '\n' '\0' \`"
   allfiles=`find . maxdepth $depth -type f -print0 | xargs -0 md5sum | sort |tr '\n' '\0' `
 fi
 echo "uniqfiles=\`echo \$allfiles | uniq -w 32 |tr '\n' '\0' \`"
-uniqfiles=`echo $allfiles | uniq -w 32|tr '\n' '\0'`
+uniqfiles=`echo $allfiles | uniq -w 32`
 
 echo md5_all="\`comm  <(echo \$allfiles) <(echo \$uniqfiles) -2 -3 | cut -c -32\ | tr '\n' '\0' \`"
-md5_all=`comm  <(echo $allfiles) <(echo $uniqfiles) -2 -3 | cut -c -32|tr '\n' '\0'`
+md5_all=`comm  <(echo $allfiles) <(echo $uniqfiles) -2 -3 | cut -c -32`
 
 #echo "uniq_all_files=\`echo \$md5_all |xargs -I {}  grep {} <(echo \$allfiles) |tr '\n' '\0' \`"
 echo $md5_all |xargs -I {}  grep {} <(echo $allfiles) >$output_file
 echo "结果已保存到 $output_file 文件。"
+
+
+find /mnt/e/BAKKK/常用文档 -type f -print0 | xargs -0 md5sum | sort > 1.txt
+cat 1.txt | uniq -w 32 > 2.txt
+comm  1.txt 2.txt -2 -3 | cut -c -32 >3.txt
+cat 3.txt |xargs -I {}  grep {} 1.txt > 4.txt
+
+
+(ls) |cut -c -8 |xargs -I {} unzip -p '{}*zip' '*.csv' | while read file; do mv "$file" "${file%.*}.csv"; done
